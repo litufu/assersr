0e64ddaf59ae3b6831147ac7ee4b62d0ce99d64e
litufu <285212818@qq.com>
2019-04-13 11:05:36 +0800

    限制地方群人数超过300人

--- src/resolvers/LocationGroup.js
+++ src/resolvers/LocationGroup.js
@@ -1,13 +1,13 @@
 export const LocationGroup = {
     users: async (parent, args, ctx) => {
-      // const usersCount = await ctx.db
-      // .usersConnection({where:{locationGroups_some:{id:parent.id}}})
-      // .aggregate()
-      // .count()
-      // if (usersCount>100){
-      //   // 人数大于300不在返回具体人员信息
-      //   return []
-      // }
+      const usersCount = await ctx.db
+      .usersConnection({where:{locationGroups_some:{id:parent.id}}})
+      .aggregate()
+      .count()
+      if (usersCount>300){
+        // 人数大于300不在返回具体人员信息
+        return []
+      }
       return ctx.db.locationGroup({ id: parent.id }).users({first:100})
     },
     messages:async (parent, args, ctx) =>{
